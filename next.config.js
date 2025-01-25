55a818d5ae454d9d9e98dcf7c7653aafbb3901c6
Tommi Laukkanen <tlaukkanen@users.noreply.github.com>
2025-01-25 17:36:20 +0200

    Add Next.js configuration file with environment variables for App Insights and version tag

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  env: {
+    NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING: process.env.NEXT_PUBLIC_APPINSIGHTS_CONNECTION_STRING,
+    NEXT_PUBLIC_VERSION_TAG: process.env.NEXT_PUBLIC_VERSION_TAG,
+  },
+}
+
+module.exports = nextConfig;
